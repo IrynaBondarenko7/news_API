95267f6fd7bcbfc3c6d314eb040a179d9ec98c36
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-08-23 16:14:49 +0100

    create /api/topics endpoint & test 200 status

---
+++ __tests__/app.test.js
@@ -0,0 +1,23 @@
+const app = require("../app");
+const request = require("supertest");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  test("GET:200 sends an array of topic objects, each of which should have the following properties: slug, description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.topics.length).toBe(3);
+        body.topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug");
+          expect(topic).toHaveProperty("description");
+        });
+      });
+  });
+});
