6a7cfd4d4a8027568ccd03e7ef9f9957090034a5
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-08-26 11:12:36 +0100

    create /api/articles/:article_id endpoint & test 200 status

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -44,3 +44,24 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("GET:200 sends a single article to the client", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+});
--- controllers/endpoins.controllers.js
+++ controllers/endpoins.controllers.js
@@ -3,7 +3,6 @@ const { readEndpoints } = require("../models/endpoints.models");
 exports.getEndpoints = (req, res) => {
   readEndpoints()
     .then((endpoints) => {
-      console.log(endpoints);
       res.status(200).send(endpoints);
     })
     .catch((err) => {
