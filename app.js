2b666d19cb592037b1729cdd1cac86dfd01cdc1c
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-08-26 10:35:46 +0100

    solve task 3 - get /api

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const { getTopics } = require("./controllers/topics.controllers");
+const { getEndpoints } = require("./controllers/endpoins.controllers");
 
 const express = require("express");
 const app = express();
 
 app.get("/api/topics", getTopics);
+app.get("/api", getEndpoints);
 
 app.use((req, res, next) => {
   res.status(404).send({ msg: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
 });
 
 module.exports = app;
